eb76f354ecf81d820e37eba37d4ee0b6dd05856f
Arthur Crippa Búrigo <arthurcburigo@gmail.com>
2018-05-16 19:27:22 -0300

    Added response mappings for several classes.

--- bgapi/base.py
+++ bgapi/base.py
@@ -7,30 +7,91 @@ from bgapi import (MessageType, MessageClass, Error)
 PARSE_MAP = {
     MessageType.COMMAND_RESPONSE: {
         MessageClass.COEX: {
+            0x01: bgapi.coex.rsp.get_counters,
+            0x00: bgapi.coex.rsp.set_options,
         },
 
         MessageClass.DFU: {
+            0x01: bgapi.dfu.rsp.flash_set_address,
+            0x02: bgapi.dfu.rsp.flash_upload,
+            0x03: bgapi.dfu.rsp.flash_upload_finish,
         },
 
         MessageClass.ENDPOINT: {
         },
 
         MessageClass.FLASH: {
+            0x04: bgapi.flash.rsp.ps_erase,
+            0x01: bgapi.flash.rsp.ps_erase_all,
+            0x03: bgapi.flash.rsp.ps_load,
+            0x02: bgapi.flash.rsp.ps_save,
         },
 
         MessageClass.GATT: {
+            0x03: bgapi.gatt.rsp.discover_characteristics,
+            0x04: bgapi.gatt.rsp.discover_characteristics_by_uuid,
+            0x06: bgapi.gatt.rsp.discover_descriptors,
+            0x01: bgapi.gatt.rsp.discover_primary_services,
+            0x02: bgapi.gatt.rsp.discover_primary_services_by_uuid,
+            0x0c: bgapi.gatt.rsp.execute_characteristic_value_write,
+            0x10: bgapi.gatt.rsp.find_included_services,
+            0x13: bgapi.gatt.rsp.prepare_characteristic_value_reliable_write,
+            0x0b: bgapi.gatt.rsp.prepare_characteristic_value_write,
+            0x07: bgapi.gatt.rsp.read_characteristic_value,
+            0x08: bgapi.gatt.rsp.read_characteristic_value_by_uuid,
+            0x12: bgapi.gatt.rsp.read_characteristic_value_from_offset,
+            0x0e: bgapi.gatt.rsp.read_descriptor_value,
+            0x11: bgapi.gatt.rsp.read_multiple_characteristic_values,
+            0x0d: bgapi.gatt.rsp.send_characteristic_confirmation,
+            0x05: bgapi.gatt.rsp.set_characteristic_notification,
+            0x00: bgapi.gatt.rsp.set_max_mtu,
+            0x09: bgapi.gatt.rsp.write_characteristic_value,
+            0x0a: bgapi.gatt.rsp.write_characteristic_value_without_response,
+            0x0f: bgapi.gatt.rsp.write_descriptor_value,
         },
 
         MessageClass.GATT_SERVER: {
+            0x06: bgapi.gatt_server.rsp.find_attribute,
+            0x01: bgapi.gatt_server.rsp.read_attribute_type,
+            0x00: bgapi.gatt_server.rsp.read_attribute_value,
+            0x05: bgapi.gatt_server.rsp.send_characteristic_notification,
+            0x03: bgapi.gatt_server.rsp.send_user_read_response,
+            0x04: bgapi.gatt_server.rsp.send_user_write_response,
+            0x08: bgapi.gatt_server.rsp.set_capabilities,
+            0x02: bgapi.gatt_server.rsp.write_attribute_value,
         },
 
         MessageClass.HARDWARE: {
+            0x0c: bgapi.hardware.rsp.set_lazy_soft_timer,
+            0x00: bgapi.hardware.rsp.set_soft_timer,
         },
 
         MessageClass.LE_CONNECTION: {
+            0x04: bgapi.le_connection.rsp.close,
+            0x02: bgapi.le_connection.rsp.disable_slave_latency,
+            0x01: bgapi.le_connection.rsp.get_rssi,
+            0x00: bgapi.le_connection.rsp.set_parameters,
+            0x03: bgapi.le_connection.rsp.set_phy,
         },
 
         MessageClass.LE_GAP: {
+            0x0c: bgapi.le_gap.rsp.bt5_set_adv_data,
+            0x13: bgapi.le_gap.rsp.clear_advertise_configuration,
+            0x1a: bgapi.le_gap.rsp.connect,
+            0x03: bgapi.le_gap.rsp.end_procedure,
+            0x0f: bgapi.le_gap.rsp.set_advertise_channel_map,
+            0x12: bgapi.le_gap.rsp.set_advertise_configuration,
+            0x11: bgapi.le_gap.rsp.set_advertise_phy,
+            0x10: bgapi.le_gap.rsp.set_advertise_report_scan_request,
+            0x0e: bgapi.le_gap.rsp.set_advertise_timing,
+            0x05: bgapi.le_gap.rsp.set_conn_parameters,
+            0x19: bgapi.le_gap.rsp.set_data_channel_classification,
+            0x16: bgapi.le_gap.rsp.set_discovery_timing,
+            0x17: bgapi.le_gap.rsp.set_discovery_type,
+            0x0d: bgapi.le_gap.rsp.set_privacy_mode,
+            0x14: bgapi.le_gap.rsp.start_advertising,
+            0x18: bgapi.le_gap.rsp.start_discovery,
+            0x15: bgapi.le_gap.rsp.stop_advertising,
         },
 
         MessageClass.SM: {
