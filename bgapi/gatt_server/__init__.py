618e171702578a60fb78cec88c5d44b7f18d0640
Arthur Crippa Búrigo <arthurcburigo@gmail.com>
2018-05-16 15:00:55 -0300

    Added gatt_server events.

--- bgapi/gatt_server/__init__.py
+++ bgapi/gatt_server/__init__.py
@@ -1,3 +1,4 @@
 import bgapi.gatt_server.cmd
 import bgapi.gatt_server.rsp
+import bgapi.gatt_server.evt
 from bgapi.gatt_server.types import *
---
+++ bgapi/gatt_server/evt.py
@@ -0,0 +1,89 @@
+from struct import (unpack_from, calcsize)
+
+from bgapi.gatt_server.types import CharacteristicStatusFlag
+from bgapi.gatt.types import (ClientConfigFlag, AttOpcode)
+from bgapi.base import _parse_result
+
+
+def attribute_value(data: bytes, offset: int = 0):
+    FORMAT = '<BHBHB'
+    connection, attribute, att_opcode, offset, n = unpack_from(
+        FORMAT, data, offset=offset)
+    offset += calcsize(FORMAT)
+    value = data[offset:offset + n]
+    offset += n
+
+    payload = {
+        'connection': connection,
+        'attribute': attribute,
+        'att_opcode': AttOpcode(att_opcode),
+        'offset': offset,
+        'value': value,
+    }
+
+    return payload, offset
+
+
+def characteristic_status(data: bytes, offset: int = 0):
+    FORMAT = '<BHBH'
+    connection, characteristic, status_flags, client_config_flags = unpack_from(
+        FORMAT, data, offset=offset)
+    offset += calcsize(FORMAT)
+
+    payload = {
+        'connection': connection,
+        'characteristic': characteristic,
+        'status_flags': CharacteristicStatusFlag(status_flags),
+        'client_config_flags': ClientConfigFlag(client_config_flags),
+    }
+
+    return payload, offset
+
+
+def execute_write_completed(data: bytes, offset: int = 0):
+    FORMAT = '<B'
+    connection, = unpack_from(FORMAT, data, offset=offset)
+    offset += calcsize(FORMAT)
+    result, offset = _parse_result(data, offset)
+
+    payload = {
+        'connection': connection,
+        'result': result,
+    }
+
+    return payload, offset
+
+
+def user_read_request(data: bytes, offset: int = 0):
+    FORMAT = '<BHBH'
+    connection, characteristic, att_opcode, offset = unpack_from(
+        FORMAT, data, offset=offset)
+    offset += calcsize(FORMAT)
+
+    payload = {
+        'connection': connection,
+        'characteristic': characteristic,
+        'att_opcode': AttOpcode(att_opcode),
+        'offset': offset,
+    }
+
+    return payload, offset
+
+
+def user_write_request(data: bytes, offset: int = 0):
+    FORMAT = '<BHBHB'
+    connection, characteristic, att_opcode, offset, n = unpack_from(
+        FORMAT, data, offset=offset)
+    offset += calcsize(FORMAT)
+    value = data[offset:offset + n]
+    offset += n
+
+    payload = {
+        'connection': connection,
+        'characteristic': characteristic,
+        'att_opcode': AttOpcode(att_opcode),
+        'offset': offset,
+        'value': value,
+    }
+
+    return payload, offset
