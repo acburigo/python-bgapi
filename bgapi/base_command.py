8afd7e1d60a0ad8f894ec0c915213187657a8f20
Arthur Crippa Búrigo <arthurcburigo@gmail.com>
2018-05-21 18:27:54 -0300

    Fix variable length packets.

--- bgapi/base_command.py
+++ bgapi/base_command.py
@@ -2,6 +2,6 @@ from struct import pack
 
 
 def command(msg_type, min_payload_length, msg_class, msg_id, payload=b''):
-    b = pack('<BBBB', msg_type, min_payload_length, msg_class, msg_id)
+    b = pack('<BBBB', msg_type, len(payload), msg_class, msg_id)
     b += payload
     return b
