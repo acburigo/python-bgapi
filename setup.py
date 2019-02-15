0a42c644b183f0273a52c91244e5764eabe638f6
Leandro Heck <leoheck@gmail.com>
2019-02-15 23:48:03 +0000

    Increase version number

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
 
 setup(
     name='bgapi',
-    version='0.0.1',
+    version='0.0.2',
     description='Library for creating and parsing BGAPI packets.',
     url='https://github.com/acburigo/python-bgapi',
+    python_requires='>=3',
     license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
