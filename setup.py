ad6149ea448a2d203e0cb93bf85f17a5abfc4b8f
Leandro Heck <leoheck@gmail.com>
2019-02-15 23:55:29 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+import time
+
+PACKAGE = 'bgapi'
+BASE_VERSION = '0.0.2'
+IS_RELEASED = True
+version = BASE_VERSION
+
+if not IS_RELEASED:
+    version = "{}.dev{}".format(BASE_VERSION, int(time.time()))
 
 setup(
     name='bgapi',
-    version='0.0.2',
+    version=version,
     description='Library for creating and parsing BGAPI packets.',
     url='https://github.com/acburigo/python-bgapi',
     python_requires='>=3',
@@ -17,7 +26,10 @@ setup(
     ],
     keywords=['BGAPI', 'protocol', 'encode', 'decode', 'ble',
               'bluetooth low energy'],
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    #packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    zip_safe=False,
     author='Arthur Crippa Búrigo',
     author_email='arthurcburigo@gmail.com',
 )
